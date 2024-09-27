31f2d0f4d9e2451e287a9b44b4e9d7456b5b2bb0
Cuong <cuonghuynh1009@gmail.com>
2024-09-27 20:25:26 +0700

    fix uneven card size

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import Header from "./Components/Header"
+import CardLeft from "./Components/CardLeft";
+import CardRight from "./Components/CardRight";
+import Header from "./Components/Header";
 // import ChoiceButton from "./Components/ChoiceButton"
 
 function App() {
   return (
     <>
-    <Header />
+      <Header />
+      <div className="body">
+        <span className="left-body">
+          <CardLeft />
+        </span>
+        <span className="right-body">
+          <CardRight />
+        </span>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/CardLeft.jsx
@@ -0,0 +1,12 @@
+function CardLeft(){
+    return(
+        <div className="card-left">
+            {/* <img className="card-img" src={pfp} alt="Twitch Chatter pfp"></img> */}
+            <h2 className='card-left-title'>Card Left</h2>
+            <p className='card-left-description'>Card Left Description</p>
+        </div>
+        
+    )
+}
+
+export default CardLeft 
