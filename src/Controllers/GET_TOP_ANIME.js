8cd677e0e19dd0d61a371528a4ad29526ab3faf5
Cuong <cuonghuynh1009@gmail.com>
2024-10-01 20:53:11 +0700

    update more pages to fecth

--- src/Controllers/GET_TOP_ANIME.js
+++ src/Controllers/GET_TOP_ANIME.js
@@ -1,19 +1,32 @@
 import axios from "axios";
 // const dotenv = require('dotenv').config()
-const TopAnimeURL = `https://api.jikan.moe/v4/top/anime?top_anime_filter="bypopularity"`
+const TopAnimeURL0 = `https://api.jikan.moe/v4/top/anime?top_anime_filter="bypopularity"&page=1`;
+const TopAnimeURL1 = `https://api.jikan.moe/v4/top/anime?top_anime_filter="bypopularity"&page=2`;
+const TopAnimeURL2 = `https://api.jikan.moe/v4/top/anime?top_anime_filter="bypopularity"&page=3`;
 
-async function GET_TOP_ANIME(){
-    try{
-        const response = await axios.get(TopAnimeURL);
-        if(!response){
-            throw new Error("Something went wrong while fetching xdd")
-        }
-        else return response.data.data;
-    }
-    catch(error){
-        console.log("Error fetching data:", error);
-        return null;
-    }
+function randomLink() {
+  const choose = Math.floor(Math.random() * 2);
+  switch (choose) {
+    case 0:
+        return TopAnimeURL0;
+    case 1:
+        return TopAnimeURL1;
+    case 2:
+        return TopAnimeURL2;
+  }
 }
 
-export{GET_TOP_ANIME};
+async function GET_TOP_ANIME() {
+  try {
+    const response = await axios.get(randomLink());
+    if (!response) {
+      throw new Error("Something went wrong while fetching xdd");
+    } else return response.data.data;
+  } catch (error) {
+    console.log("Error fetching data:", error);
+    return null;
+  }
+}
+
+
+export { GET_TOP_ANIME};
