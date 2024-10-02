b53f099d90fc12433c81f7b5f49a36196e3f1a34
cuongh1799 <cuongghuynhh1009@gmail.com>
2024-10-02 22:16:36 +0700

    refactor fetch function, now with more pages! and randomness

--- src/Controllers/GET_TOP_ANIME.js
+++ src/Controllers/GET_TOP_ANIME.js
@@ -1,24 +1,15 @@
 import axios from "axios";
-// const dotenv = require('dotenv').config()
-const TopAnimeURL0 = `https://api.jikan.moe/v4/top/anime?top_anime_filter="bypopularity"&page=1`;
-const TopAnimeURL1 = `https://api.jikan.moe/v4/top/anime?top_anime_filter="bypopularity"&page=2`;
-const TopAnimeURL2 = `https://api.jikan.moe/v4/top/anime?top_anime_filter="bypopularity"&page=3`;
 
-function randomLink() {
-  const choose = Math.floor(Math.random() * 2);
-  switch (choose) {
-    case 0:
-        return TopAnimeURL0;
-    case 1:
-        return TopAnimeURL1;
-    case 2:
-        return TopAnimeURL2;
-  }
+function randomPage() {
+  const choose = Math.floor(Math.random() * 10 + 1);
+  return choose;
 }
 
 async function GET_TOP_ANIME() {
   try {
-    const response = await axios.get(randomLink());
+    const response = await axios.get(
+      `https://api.jikan.moe/v4/top/anime?top_anime_filter="bypopularity"&page=${randomPage()}`
+    );
     if (!response) {
       throw new Error("Something went wrong while fetching xdd");
     } else return response.data.data;
@@ -28,5 +19,4 @@ async function GET_TOP_ANIME() {
   }
 }
 
-
-export { GET_TOP_ANIME};
+export { GET_TOP_ANIME };
