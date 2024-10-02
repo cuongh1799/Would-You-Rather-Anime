3eb699981c5ddd093f682d689205f63946bb27f8
cuongh1799 <cuongghuynhh1009@gmail.com>
2024-10-02 22:16:58 +0700

    added loading jsx and css

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,18 @@
+import "../css/Loading.css"
+function Loading() {
+  return (
+    <>
+      <div className="header">
+        <div className="loading">
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+        </div>
+        <h1>Loading!</h1>
+    </div>
+    </>
+  );
+}
+
+export default Loading;
