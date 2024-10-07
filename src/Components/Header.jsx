dd035c69cb87f82419ab2e0047daf0e7e76cabe3
cuongh1799 <cuongghuynhh1009@gmail.com>
2024-10-07 17:12:09 +0700

    add play to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,21 @@
-function Header(){
-    return(
-        <header className="header-bar">
-            <a className="header-logo">Would you rather</a>
-            <nav className="nav-bar">
-                <a className="header-nav">Home</a>
-                <a className="header-nav">About</a>
-                <a className="header-nav">Profile</a>
-            </nav>
-        </header>
-    )
+import { Link, Outlet } from "react-router-dom";
+
+function Header() {
+  return (
+    <>
+      <header className="header-bar">
+        <a className="header-logo">
+          Would you rather
+        </a>
+        <nav className="nav-bar">
+          <Link to="/Play"className="header-nav">Play</Link>
+          <a className="header-nav">Home</a>
+          <Link to="/About"className="header-nav">About</Link>
+          <a className="header-nav">Profile</a>
+        </nav>
+      </header>
+      <Outlet />
+    </>
+  );
 }
-export default Header
+export default Header;
