57a300a412af4745043a5344136b5d4f2cb1ca83
cuongh1799 <cuongghuynhh1009@gmail.com>
2024-10-07 21:29:49 +0700

    update on about page

--- routes/About.jsx
+++ routes/About.jsx
@@ -1,23 +1,34 @@
 import doro from "../public/doro.png";
 import banner from "../public/banner.jpg";
+import banner2 from "../public/banner2.jpg";
 function About() {
   return (
-    <div className="about-body">
-      <img className="banner" src={banner} />
-      <div className="about-info">
-        <h1 className="about-title">Would You Rather Anime Version</h1>
-        <p className="about-description">
-          Would you rather Anime version was inspired by the tradtional "Would
-          you prefer" web game.
-        </p>
-        <p className="about-description">
-          The project was built using Vite ReactJs, axios for fetching Jikan
-          API, CSS for styling.
-        </p>
+    <>
+      <div className="about-body">
+        <div className="banner-wrapper">
+          <img className="banner" src={banner} />
+          <div className="about-info">
+            <h1 className="about-title">Would You Rather Anime Version</h1>
+            <p className="about-description">
+              Would you rather Anime version was inspired by the tradtional
+              "Would you prefer" web game.
+            </p>
+            <p className="about-description">
+              The project was built using cuongh1799, you can check how this
+              work on his Github!
+            </p>
+          </div>
+        </div>
       </div>
-
-      {/* <img src={doro}/> */}
-    </div>
+      <div className="about-stacks">
+        <div className="banner-wrapper">
+          <img className="banner" src={banner2} />
+          <div className="about-info">
+            <h1 className="about-title">Tech Stacks</h1>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
