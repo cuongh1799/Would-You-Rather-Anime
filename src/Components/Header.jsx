b4405d8a94711c26f02a8289d1c0ee540e59ab34
cuongh1799 <cuongghuynhh1009@gmail.com>
2024-10-21 21:04:51 +0700

    test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,37 @@
 import { Link, Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Header() {
+  const [logInState, setLogInState] = useState(false);
+
+  // Check Token
+  useEffect(() => {
+    const checkToken = localStorage.getItem("token");
+    if (checkToken === undefined) {
+      setLogInState(false);
+    } else setLogInState(true);
+  }, []);
+
   return (
     <>
       <header className="header-bar">
-        <a className="header-logo">
-          Would you rather
-        </a>
+        <a className="header-logo">Would you rather</a>
         <nav className="nav-bar">
-          <Link to="/Play"className="header-nav">Play</Link>
-          <Link to="/"className="header-nav">Home</Link>
-          <Link to="/login" className="header-login">
-            <div className="header-login-content">Log in</div>
+          <Link to="/Play" className="header-nav">
+            Play
           </Link>
+          <Link to="/" className="header-nav">
+            Home
+          </Link>
+          {logInState ? (
+            <Link to="/login" className="header-login">
+              <div className="header-login-content">Yep</div>
+            </Link>
+          ) : (
+            <Link to="/login" className="header-login">
+              <div className="header-login-content">Login</div>
+            </Link>
+          )}
         </nav>
       </header>
       <Outlet />
